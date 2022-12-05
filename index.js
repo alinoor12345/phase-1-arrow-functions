fa0f7ee330d520f64e6a80866ece9c19ee750538
Ali Omar <ali.noor.student.moringaschool.com git config --gobal Ali abdullahigit config --global user.email ali.noor.student.moringaschool.com>
2022-12-05 11:56:20 +0300

    lab done

---
+++ index.js
@@ -0,0 +1,12 @@
+//takes 2 parameters and divides the first parameter by the 
+function divide(a = 2000, b = 100){
+    return (a/b)
+}
+//takes a parameter and squares it
+const square = (a = 4) => {
+    return (a*a);
+}
+
+const add = (a, b) => {
+    return (a+b)
+}
